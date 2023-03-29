3e67ae29f30f25d25f643598dde9b1856e4bd16c
colecrum26 <cwcrumrine@gmail.com>
2023-03-28 20:50:06 -0500

    refactored some styling

--- sticky.js
+++ sticky.js
@@ -6,7 +6,7 @@ let sticky = navbar.offsetTop;
 
 function stickyNav(){
     if (window.scrollY>= sticky){
-        navbar.classList.add("sticky")
+        navbar.classList.add("sticky");
     } else {
         navbar.classList.remove("sticky");
     }
